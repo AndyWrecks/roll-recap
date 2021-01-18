c06c6158892cf671a8b49400ee24ee77d187643f
Andy Reks <andiskreks@gmail.com>
2021-01-18 11:09:14 -0500

    Getting Valid Roll, Defining todo

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,30 @@
-import { filter } from "lodash";
+import { filter, map } from "lodash";
 
-function getAllRolls(allChatMessages: Messages) {
-  const roll: any = filter(
-    allChatMessages.entities,
-    (chatMessage: ChatMessage) => {
-      if (chatMessage.isRoll) {
-        return chatMessage.data;
-      }
+function getAllRollMessages(allChatMessages: Messages): any[] {
+  return filter(allChatMessages.entities, (chatMessage: ChatMessage) => {
+    if (chatMessage.isRoll) {
+      return chatMessage.roll;
     }
-  );
-  return roll;
-}
-
-function component() {
-  Hooks.on("ready", function () {
-    const allChatMessages: Messages = game.messages;
-    const rolls = getAllRolls(allChatMessages);
-
-    console.log(rolls);
   });
 }
 
-component();
+function getRollDataFromMessages(chatMessages: ChatMessage[]) {
+  return map(chatMessages, (chatMessage) => chatMessage.roll);
+}
+
+// TODO:
+// Create popup to display rollData
+// Breakdown data by user
+// Breakdown data by actor
+// Breakdown data by game session
+// Expose data for CG dashboard
+// Get code on github
+
+Hooks.on("ready", function () {
+  const allChatMessages: Messages = game.messages;
+  const rolls: ChatMessage[] = getAllRollMessages(allChatMessages);
+  const rollData: Roll[] = getRollDataFromMessages(rolls);
+
+  console.log("rolls", rolls);
+  console.log("rollData", rollData);
+});
